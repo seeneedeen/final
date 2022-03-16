52f6aa9aa1a9455ab9cd95b71a377b7ca2033d39
Sittisak Bannob <sitthisak2557@gmail.com>
2022-03-16 14:12:07 +0700

    splite FB login to components

--- web/src/App.js
+++ web/src/App.js
@@ -1,60 +1,11 @@
-import axios from 'axios';
-import FacebookLogin from "react-facebook-login"
 import './App.css';
-import React,{useState} from 'react';
+import UserFB from './components/UserFB';
 
 function App() {
 
-  let showProfile,loginBtn;
-
-  const [profile,setPicfile] = useState({
-    name : "",
-    picture : "",
-    status : false
-  })
-
-  const responseFacebook = async (response) => {
-    if(response.accessToken)
-    {
-      let result = await axios.post('http://localhost:8080/login', {
-        token: response.accessToken
-      })
-      sessionStorage.setItem('access_token', result.data.access_token)
-      setPicfile({
-        name:result.data.result.name,
-        picture:result.data.result.picture.data.url,
-        status:true
-      })
-    }
-  }
-
-  if(profile.status){
-    loginBtn = (<></>)
-    showProfile = (
-      <>
-        <h1>{profile.name}</h1>
-        <img src={profile.picture} alt="Logo" />
-      </>
-    )
-  }else{
-    loginBtn = (
-      <FacebookLogin
-        appId = "1075683823003347"
-        autoLoad = {true}
-        callback = {responseFacebook}
-      />
-    )
-    showProfile = (
-      <>
-        <h1>Please Login</h1>
-      </>
-    )
-  }
-
   return (
     <div className="App">
-      <>{loginBtn}</>
-      <>{showProfile}</>
+      <UserFB />
     </div>
   );
 }
